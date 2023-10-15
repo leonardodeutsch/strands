e564fc73055fcb7ab4c6e824405c19b78194db16
Leo <deutschleonardo@gmail.com>
2023-10-15 16:42:21 -0500

    fixed wrong clerkprovider import from react instead of nextjs

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,4 +1,4 @@
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 import '../globals.css';
@@ -12,7 +12,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export default function RootLayout({ children }: { children: React.ReactNode}) {
   return (
-    <ClerkProvider frontendApi="your_frontend_api_key_here">
+    <ClerkProvider>
       <html lang="en">
         <body className={`${inter.className} bg-dark-1`}>
           {children}
