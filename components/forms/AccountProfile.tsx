e2a002d30f40845282e81840ed2172d14bf599b4
Leo <deutschleonardo@gmail.com>
2023-10-16 14:06:20 -0500

    implemented uploadthing to upload user profile pictures

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,6 +17,9 @@ import { Button } from '../ui/button';
 import Image from 'next/image';
 import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
+import { isBase64Image } from '@/lib/utils';
+import { useUploadThing } from '@/lib/uploadthing';
+
 
 interface Props {
   user: {
@@ -32,6 +35,7 @@ interface Props {
 
 const AccountProfile = ({ user, btnTitle } : Props) => {
   const [files, setFiles] = useState<File[]>([]);
+  const { startUpload } = useUploadThing("media");
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -60,10 +64,18 @@ const AccountProfile = ({ user, btnTitle } : Props) => {
     }
   }
 
-  function onSubmit(values: z.infer<typeof UserValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
+  const onSubmit = async (values: z.infer<typeof UserValidation>) => {
+    const blob = values.profile_photo;
+    const hasImageChanged = isBase64Image(blob);
+    
+    if (hasImageChanged) {
+      const imgRes = await startUpload(files);
+      if (imgRes && imgRes[0].fileUrl) {
+        values.profile_photo = imgRes[0].fileUrl;
+      }
+    }
+
+    // TODO: Update user profile
   }
   
   return (
