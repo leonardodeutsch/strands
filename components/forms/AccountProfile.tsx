cfd96dff72caf6f0661cc77d82668765ceb8840e
Leo <deutschleonardo@gmail.com>
2023-10-16 15:15:05 -0500

    implemented database connection and upload of user info

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -19,6 +19,8 @@ import { ChangeEvent, useState } from 'react';
 import { Textarea } from '../ui/textarea';
 import { isBase64Image } from '@/lib/utils';
 import { useUploadThing } from '@/lib/uploadthing';
+import { usePathname, useRouter } from 'next/navigation';
+import { updateUser } from '@/lib/actions/user.actions';
 
 
 interface Props {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle } : Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm({
     resolver: zodResolver(UserValidation),
@@ -75,7 +79,20 @@ const AccountProfile = ({ user, btnTitle } : Props) => {
       }
     }
 
-    
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname
+    });
+
+    if (pathname === '/profile/edit') {
+      router.back();
+    } else {
+      router.push('/')
+    }
   }
   
   return (
