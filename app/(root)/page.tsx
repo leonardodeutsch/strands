f164980ca2373493ae169c60036ecbd1b8425b7b
Leo <deutschleonardo@gmail.com>
2023-10-17 11:39:00 -0500

    implemented fetching strands and pagination on home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 
  
-export default function Home() {
+export default async function Home() {
+  // const result = await fetchStrands()
   return (
     <div>
       <h1 className="head-text text-left">Home</h1>
--- lib/actions/strand.actions.ts
+++ lib/actions/strand.actions.ts
@@ -6,6 +6,7 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose"
 import { StrandValidation } from "../validations/strand";
 
+
 interface Params {
   text: string,
   author: string,
@@ -27,4 +28,30 @@ export async function createStrand({ text, author, communityId, path }: Params) 
   });
 
   revalidatePath(path);
+}
+
+export async function fetchStrands(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+  const skipAmount = (pageNumber - 1) * pageSize;
+  const strandsQuery = Strand
+    .find({ parentId: {$in: [null, undefined]}})
+    .sort({ createdAt: 'desc'})
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User})
+    .populate({ 
+      path: 'children', 
+      populate: {
+        path: 'author',
+        model: User,
+        select: "_id name parentId image"
+      }
+    })
+
+    const totalStrandsCount = await Strand.countDocuments({ parentId: {$in: [null, undefined]} });
+    const strands = await strandsQuery.exec();
+
+    const isNext = totalStrandsCount > skipAmount + strands.length;
+    
+    return { strands, isNext };
 }
